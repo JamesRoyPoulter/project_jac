dffb91c3d167e7df1ebe3d7ac916cd7d56c37641
Andrea Kennedy <asrkennedy@gmail.com>
2013-09-03 10:12:30 +0100

    fixing bug with checkin/video in checkin json

--- app/serializers/checkin_serializer.rb
+++ app/serializers/checkin_serializer.rb
@@ -2,10 +2,9 @@ class CheckinSerializer < ActiveModel::Serializer
   attributes :id, :latitude, :longitude, :title, :description, :user_id, :address,
              :created_at, :categories, :video, :audio, :image
 
-
-  # def video
-  #   Video.where(checkin_id: self.id)
-  # end
+  def video
+    Video.where(checkin_id: self.id)
+  end
 
   def audio
     Audio.where(checkin_id: self.id)
